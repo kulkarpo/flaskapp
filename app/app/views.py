57e6728cd70eebe0e5aa61f2035b8b81800c021f
kulkarpo <r.kpooja12@gmail.com>
2020-07-16 15:11:00 +0200

    Learning flask : ep 11, 12 and 13
    Signed-off-by: kulkarpo <r.kpooja12@gmail.com>

--- app/app/views.py
+++ app/app/views.py
@@ -3,6 +3,8 @@ from flask import render_template
 from datetime import datetime
 from flask import redirect, request
 from flask import jsonify, make_response
+import os
+from werkzeug.utils import secure_filename
 
 @app.route("/")
 def index():
@@ -175,9 +177,76 @@ def guestbook():
 def create_entry():
 
     req = request.get_json()
-
     print(req)
 
     res = make_response(jsonify({"message": "OK"}), 200)
-
     return res
+
+@app.route("/query")
+def query():
+    """args = request.args
+    print(args)
+
+    for k, v in args.items():
+        print(k + ":" + v)
+        print(f"{k} : {v}")"""
+
+    print(app.config)
+
+    if request.args:
+        args = request.args
+        serialised = "".join(f"{k} : {v}" for k,v in args.items())
+        return f"(Query) {serialised}", 200
+    return "No query strings recieved", 200
+
+
+app.config["IMAGE_UPLOADS"] = "/Users/pk/Documents/Studies/selflearn/flaskapp/app/app/static/img/uploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+app.config['MAX_IMAGE_FILESIZE'] = 50 * 1024 * 1024
+
+
+def allowed_image_filesize(filesize):
+
+    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+        return True
+    else:
+        return False
+
+
+def allowed_imagetype(image_filename):
+    if "." in image_filename:
+        ext = image_filename.rsplit(".", 1)[1]
+
+        if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+            return True
+    return False
+
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == 'POST':
+        if request.files:
+            if "filesize" in request.cookies:
+                if not allowed_image_filesize(request.cookies["filesize"]):
+                    print("Filesize exceeded maximum limit")
+                    return redirect(request.url)
+
+            image = request.files["image"]
+
+            if image.filename == "":
+                print("No filename")
+                return redirect(request.url)
+
+            if allowed_imagetype(image.filename):
+                filename = secure_filename(image.filename)
+
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+                print("Image saved")
+
+                return redirect(request.url)
+
+            else:
+                print("That file extension is not allowed ")
+                return redirect(request.url)
+    return render_template("public/upload_image.html")
